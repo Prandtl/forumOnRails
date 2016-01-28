40295eb405595aee65a202ad3735b1012c0d2470
prandtl <prandtl211@gmail.com>
2016-01-28 15:10:20 +0500

    fixed comment editing updating

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,14 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment].permit(:comment))
+    @comment_params = params.require(:comment).permit(:comment)
+    if @comment_params[:comment].blank?
+      flash[:alert] = 'You can\'t have a comment without any text.'
+      redirect_to post_path(@post)
+      return
+    end
+
+    @comment = @post.comments.create(@comment_params)
     @comment.user_id = current_user.id
     @comment.save
 
@@ -20,7 +27,15 @@ class CommentsController < ApplicationController
   def update
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
-    if @comment.update
+    @comment_params = params.require(:comment).permit(:comment)
+
+    if @comment_params[:comment].blank?
+      flash.now[:alert] = 'You can\'t have a comment without any text.'
+      render 'edit'
+      return
+    end
+
+    if @comment.update(@comment_params)
       redirect_to post_path(@post)
     else
       render 'edit'
