7c581963bf5e4914756939fb2637cc48d0cb6745
prandtl <prandtl211@gmail.com>
2016-02-15 12:01:33 +0500

    adding user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
   def show
+    find_user
+  end
+
+  def ban
+    find_user
+    if (current_user.userstatus == 1 && @user.userstatus !=1)
+      @user.userstatus = -1
+      @user.save
+    end
+  end
+  helper_method :ban
+
+  def unban
+    if (current_user.userstatus == 1 && @user.userstatus == 0)
+      @user.userstatus = 0
+      @user.save
+    end
+  end
+  helper_method :unban
+
+  private
+
+  def find_user
     @user = User.find(params[:id])
   end
 end
